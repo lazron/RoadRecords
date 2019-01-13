4750239c74df35438a1d4f97abf8fd2a774b2567
Aryaman Sharma <kaushik.rohan31@gmail.com>
2019-01-13 19:22:44 +0530

    Created file Road Rage in Android Studio

---
+++ app/src/main/java/com/example/om/roadrecords/Existing_Record.kt
@@ -0,0 +1,12 @@
+package com.example.om.roadrecords
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Existing_Record : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_existing__record)
+    }
+}
---
+++ app/src/main/java/com/example/om/roadrecords/Search_Records.kt
@@ -0,0 +1,12 @@
+package com.example.om.roadrecords
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Search_Records : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search__records)
+    }
+}
